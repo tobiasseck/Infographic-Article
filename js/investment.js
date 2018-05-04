7c5c4833532ae7a117ad359d90fb4b1e82049853
Tobias Seck <tobias@seck-web.de>
2018-05-04 15:22:14 +0200

    Datensatz aktualisiert
    Alten Datensatz gelöscht und durch neuen (bis Ende Q1 2018) ersetzt.

--- js/investment.js
+++ js/investment.js
@@ -1,53 +1,85 @@
 //Set up the data
 investment = [
-{"investee": "Gamer Sensei","datum": "08\/30\/17","summe": 4.0,"investors": 3},
-{"investee": "SLIVER.tv","datum": "08\/23\/17","summe": 9.8,"investors": 8},
-{"investee": "GawkBox","datum": "08\/10\/17","summe": 3.7,"investors": 4},
-{"investee": "Smash.gg","datum": "08\/02\/17","summe": 11.0,"investors": 5},
-{"investee": "FanAI","datum": "07\/27\/17","summe": 1.8,"investors": 12},
-{"investee": "O'Gaming TV","datum": "07\/25\/17","summe": 2.5,"investors": 1},
-{"investee": "Matcherino","datum": "07\/10\/17","summe": 1.5,"investors": 3},
-{"investee": "Super League Gaming","datum": "06\/29\/17","summe": 15.0,"investors": 4},
-{"investee": "rfrsh entertainment","datum": "06\/28\/17","summe": 8.1,"investors": 3},
-{"investee": "Pocket Gems","datum": "05\/11\/17","summe": 90.0,"investors": 1},
-{"investee": "DOJO MADNESS","datum": "04\/05\/17","summe": 6.0,"investors": 5},
-{"investee": "SLIVER.tv","datum": "03\/17\/17","summe": 16.0,"investors": 5},
-{"investee": "Wavedash Games","datum": "03\/02\/17","summe": 6.0,"investors": 7},
-{"investee": "Challengermode","datum": "02\/24\/17","summe": 1.3,"investors": 3},
-{"investee": "rfrsh entertainment","datum": "12\/22\/16","summe": 4.32,"investors": 3},
-{"investee": "boom.tv","datum": "12\/13\/16","summe": 3.5,"investors": 9},
-{"investee": "Super League Gaming","datum": "10\/26\/16","summe": 5,"investors": 2},
-{"investee": "Critical Force","datum": "09\/05\/16","summe": 4.46,"investors": 1},
-{"investee": "SLIVER.tv","datum": "08\/24\/16","summe": 6.2,"investors": 6},
-{"investee": "Ninjas in Pyjamas","datum": "08\/23\/16","summe": 0.6,"investors": 1},
-{"investee": "Wangyudashi","datum": "08\/10\/16","summe": 23,"investors": 1},
-{"investee": "Strafe","datum": "07\/15\/16","summe": 1.53,"investors": 4},
-{"investee": "Gamer Sensei","datum": "06\/09\/16","summe": 2.3,"investors": 5},
-{"investee": "DOJO MADNESS","datum": "05\/19\/16","summe": 4.5,"investors": 4},
-{"investee": "kek.tv","datum": "05\/04\/16","summe": 2.5,"investors": 11},
-{"investee": "Zengaming","datum": "04\/21\/16","summe": 2.8,"investors": 1},
-{"investee": "Super League Gaming","datum": "04\/20\/16","summe": 5.05,"investors": 10},
-{"investee": "Smash.gg","datum": "04\/01\/16","summe": 3,"investors": 2},
-{"investee": "SLIVER.tv","datum": "03\/30\/16","summe": 4,"investors": 8},
-{"investee": "Matcherino","datum": "12\/17\/15","summe": 1.25,"investors": 3},
-{"investee": "Azubu","datum": "12\/09\/15","summe": 55,"investors": 2},
-{"investee": "hitbox Entertainment","datum": "11\/02\/15","summe": 4,"investors": 3},
-{"investee": "Battle of Glory","datum": "11\/01\/15","summe": 0.1272,"investors": 2},
-{"investee": "Super League Gaming","datum": "10\/16\/15","summe": 3.25,"investors": 1},
-{"investee": "ESforce Holding","datum": "10\/15\/15","summe": 100,"investors": 1},
-{"investee": "Super League Gaming","datum": "09\/01\/15","summe": 4.5,"investors": 3},
-{"investee": "Challengermode","datum": "08\/20\/15","summe": 0.71429,"investors": 8},
-{"investee": "Matcherino","datum": "08\/07\/15","summe": 0.118,"investors": 2},
-{"investee": "Unikrn","datum": "06\/29\/15","summe": 7,"investors": 6},
-{"investee": "Strafe","datum": "06\/08\/15","summe": 0.625,"investors": 3},
-{"investee": "Super League Gaming","datum": "05\/26\/15","summe": 1.2,"investors": 10},
-{"investee": "Pocket Gems","datum": "05\/13\/15","summe": 60,"investors": 2},
-{"investee": "Alphadraft","datum": "04\/19\/15","summe": 5,"investors": 12},
-{"investee": "Unikrn","datum": "02\/04\/15","summe": 2.5,"investors": 3},
-{"investee": "Unikrn","datum": "11\/18\/14","summe": 0.35,"investors": 1},
-{"investee": "Azubu","datum": "03\/26\/14","summe": 34.5,"investors": 1},
-//{"investee": "Pocket Gems","datum": "12\/15\/10","summe": 5,"investors": 3},
-//{"investee": "Astro Gaming","datum": "01\/12\/09","summe": 3.5,"investors": 1}
+  {"investee": "Gosu.ai","datum": "	3	\/	21	\/	18	","summe": 	1.90	,"investors":	3	},
+  {"investee": "Play2Live","datum": "	3	\/	21	\/	18	","summe": 	30.00	,"investors":	1	},
+  {"investee": "Huya TV","datum": "	3	\/	9	\/	18	","summe": 	461.60	,"investors":	1	},
+  {"investee": "Douyu TV","datum": "	3	\/	8	\/	18	","summe": 	632.00	,"investors":	1	},
+  {"investee": "Vision Esports","datum": "	2	\/	22	\/	18	","summe": 	38.00	,"investors":	4	},
+  {"investee": "FanAI","datum": "	2	\/	22	\/	18	","summe": 	2.50	,"investors":	11	},
+  {"investee": "Female Legends","datum": "	2	\/	19	\/	18	","summe": 	0.66355	,"investors":	1	},
+  {"investee": "Kakao Games","datum": "	2	\/	15	\/	18	","summe": 	130.00	,"investors":	4	},
+  {"investee": "Game Digital","datum": "	2	\/	12	\/	18	","summe": 	4.50	,"investors":	1	},
+  {"investee": "Shanda Games","datum": "	2	\/	7	\/	18	","summe": 	474.00	,"investors":	1	},
+  {"investee": "Team Vitality","datum": "	2	\/	6	\/	18	","summe": 	3.10	,"investors":	3	},
+  {"investee": "Epics.gg","datum": "	1	\/	31	\/	18	","summe": 	2.00	,"investors":	6	},
+  {"investee": "Esports One","datum": "	1	\/	23	\/	18	","summe": 	3.00	,"investors":	3	},
+  {"investee": "Millennial Esports","datum": "	1	\/	15	\/	18	","summe": 	12.12	,"investors":	1	},
+  {"investee": "Chushou","datum": "	1	\/	3	\/	18	","summe": 	76.00	,"investors":	4	},
+  {"investee": "Manticore Games","datum": "	12	\/	12	\/	17	","summe": 	15.00	,"investors":	6	},
+  {"investee": "Winstrike","datum": "	11	\/	17	\/	17	","summe": 	10.00	,"investors":	2	},
+  {"investee": "OPSkins","datum": "	11	\/	13	\/	17	","summe": 	41.00	,"investors":	1	},
+  {"investee": "Unikrn","datum": "	10	\/	23	\/	17	","summe": 	40.00	,"investors":	5	},
+  {"investee": "Cloud9","datum": "	9	\/	13	\/	17	","summe": 	19.70	,"investors":	1	},
+  {"investee": "Genvid Technologies","datum": "	9	\/	11	\/	17	","summe": 	2.50	,"investors":	2	},
+  {"investee": "GAMURS","datum": "	9	\/	11	\/	17	","summe": 	3.50	,"investors":	1	},
+  {"investee": "Guilded","datum": "	9	\/	8	\/	17	","summe": 	3.20	,"investors":	10	},
+  {"investee": "Taunt","datum": "	9	\/	7	\/	17	","summe": 	1.75	,"investors":	2	},
+  {"investee": "Maestro","datum": "	9	\/	7	\/	17	","summe": 	3.00	,"investors":	3	},
+  {"investee": "Gamer Sensei","datum": "	8	\/	30	\/	17	","summe": 	4.00	,"investors":	3	},
+  {"investee": "SLIVER.tv","datum": "	8	\/	23	\/	17	","summe": 	9.80	,"investors":	8	},
+  {"investee": "Znipe.tv","datum": "	8	\/	22	\/	17	","summe": 	3.70	,"investors":	3	},
+  {"investee": "GawkBox","datum": "	8	\/	10	\/	17	","summe": 	3.70	,"investors":	4	},
+  {"investee": "Team EnVyUs","datum": "	8	\/	8	\/	17	","summe": 	35.00	,"investors":	1	},
+  {"investee": "Super Evil Megacorp","datum": "	8	\/	8	\/	17	","summe": 	19.00	,"investors":	1	},
+  {"investee": "Smash.gg","datum": "	8	\/	2	\/	17	","summe": 	11.00	,"investors":	5	},
+  {"investee": "FanAI","datum": "	7	\/	27	\/	17	","summe": 	1.80	,"investors":	12	},
+  {"investee": "O'Gaming TV","datum": "	7	\/	25	\/	17	","summe": 	2.50	,"investors":	1	},
+  {"investee": "Guilded","datum": "	7	\/	11	\/	17	","summe": 	0.12	,"investors":	1	},
+  {"investee": "Matcherino","datum": "	7	\/	10	\/	17	","summe": 	1.50	,"investors":	3	},
+  {"investee": "GameScorekeeper","datum": "	7	\/	7	\/	17	","summe": 	0.23833	,"investors":	1	},
+  {"investee": "Pandascore","datum": "	7	\/	6	\/	17	","summe": 	2.90	,"investors":	4	},
+  {"investee": "Super League Gaming","datum": "	6	\/	29	\/	17	","summe": 	15.00	,"investors":	4	},
+  {"investee": "rfrsh entertainment","datum": "	6	\/	28	\/	17	","summe": 	8.10	,"investors":	3	},
+  {"investee": "Pocket Gems","datum": "	5	\/	11	\/	17	","summe": 	90.00	,"investors":	1	},
+  {"investee": "DOJO MADNESS","datum": "	4	\/	5	\/	17	","summe": 	6.00	,"investors":	5	},
+  {"investee": "SLIVER.tv","datum": "	3	\/	17	\/	17	","summe": 	16.00	,"investors":	5	},
+  {"investee": "Wavedash Games","datum": "	3	\/	2	\/	17	","summe": 	6.00	,"investors":	7	},
+  {"investee": "Challengermode","datum": "	2	\/	24	\/	17	","summe": 	1.30	,"investors":	3	},
+  {"investee": "rfrsh entertainment","datum": "	12	\/	22	\/	16	","summe": 	4.32	,"investors":	3	},
+  {"investee": "boom.tv","datum": "	12	\/	13	\/	16	","summe": 	3.50	,"investors":	9	},
+  {"investee": "Super League Gaming","datum": "	10	\/	26	\/	16	","summe": 	5.00	,"investors":	2	},
+  {"investee": "Critical Force","datum": "	9	\/	5	\/	16	","summe": 	4.46	,"investors":	1	},
+  {"investee": "Genvid Technologies","datum": "	8	\/	29	\/	16	","summe": 	1.50	,"investors":	2	},
+  {"investee": "SLIVER.tv","datum": "	8	\/	24	\/	16	","summe": 	6.20	,"investors":	6	},
+  {"investee": "Ninjas in Pyjamas","datum": "	8	\/	23	\/	16	","summe": 	0.6	,"investors":	1	},
+  {"investee": "Wangyudashi","datum": "	8	\/	10	\/	16	","summe": 	23.00	,"investors":	2	},
+  {"investee": "Strafe","datum": "	7	\/	15	\/	16	","summe": 	1.53	,"investors":	4	},
+  {"investee": "Gamer Sensei","datum": "	6	\/	9	\/	16	","summe": 	2.30	,"investors":	5	},
+  {"investee": "DOJO MADNESS","datum": "	5	\/	19	\/	16	","summe": 	4.50	,"investors":	4	},
+  {"investee": "kek.tv","datum": "	5	\/	4	\/	16	","summe": 	2.50	,"investors":	11	},
+  {"investee": "Zengaming","datum": "	4	\/	21	\/	16	","summe": 	2.80	,"investors":	1	},
+  {"investee": "Super League Gaming","datum": "	4	\/	20	\/	16	","summe": 	5.05	,"investors":	1	},
+  {"investee": "Smash.gg","datum": "	4	\/	1	\/	16	","summe": 	3.00	,"investors":	2	},
+  {"investee": "SLIVER.tv","datum": "	3	\/	30	\/	16	","summe": 	4.00	,"investors":	8	},
+  {"investee": "Matcherino","datum": "	12	\/	17	\/	15	","summe": 	1.25	,"investors":	3	},
+  {"investee": "Azubu","datum": "	12	\/	9	\/	15	","summe": 	55.00	,"investors":	2	},
+  {"investee": "hitbox Entertainment GmbH","datum": "	11	\/	2	\/	15	","summe": 	4.00	,"investors":	3	},
+  {"investee": "Battle of Glory","datum": "	11	\/	1	\/	15	","summe": 	0.1272	,"investors":	2	},
+  {"investee": "Super League Gaming","datum": "	10	\/	16	\/	15	","summe": 	3.25	,"investors":	1	},
+  {"investee": "ESforce Holding","datum": "	10	\/	15	\/	15	","summe": 	100.00	,"investors":	1	},
+  {"investee": "Super League Gaming","datum": "	9	\/	1	\/	15	","summe": 	4.50	,"investors":	3	},
+  {"investee": "Challengermode","datum": "	8	\/	20	\/	15	","summe": 	0.71429	,"investors":	8	},
+  {"investee": "Matcherino","datum": "	8	\/	7	\/	15	","summe": 	0.118	,"investors":	2	},
+  {"investee": "Unikrn","datum": "	6	\/	29	\/	15	","summe": 	7.00	,"investors":	6	},
+  {"investee": "Strafe","datum": "	6	\/	8	\/	15	","summe": 	0.625	,"investors":	3	},
+  {"investee": "Super League Gaming","datum": "	5	\/	26	\/	15	","summe": 	1.20	,"investors":	1	},
+  {"investee": "Pocket Gems","datum": "	5	\/	13	\/	15	","summe": 	60.00	,"investors":	2	},
+  {"investee": "Alphadraft","datum": "	4	\/	19	\/	15	","summe": 	5.00	,"investors":	12	},
+  {"investee": "Unikrn","datum": "	2	\/	4	\/	15	","summe": 	2.50	,"investors":	3	},
+  {"investee": "Unikrn","datum": "	11	\/	18	\/	14	","summe": 	0.35	,"investors":	1	},
+  {"investee": "Azubu","datum": "	3	\/	26	\/	14	","summe": 	34.50	,"investors":	1	},
+  {"investee": "Pocket Gems","datum": "	12	\/	15	\/	10	","summe": 	5.00	,"investors":	3	},
+  {"investee": "Astro Gaming","datum": "	1	\/	12	\/	9	","summe": 	3.50	,"investors":	1	},
 ];
 
 //Set constants
@@ -59,7 +91,7 @@ function getDate(d) {return new Date(d.datum);}
 
 var minDate = getDate(investment[0]),
     maxDate = getDate(investment[investment.length-1]);
-            
+
 var xScale = d3.time.scale()
 .domain([maxDate, minDate])
 .range([padding, w - padding]);
@@ -122,7 +154,7 @@ svg.append("g")
     .attr("class", "yaxis")
     .attr("transform", "translate(" + padding + ",0)")
     .call(yAxis);
-    
+
 
 // titles to the axes
 svg.append("text")
@@ -133,8 +165,8 @@ svg.append("text")
 svg.append("text")
   .attr("text-anchor", "middle")
   .attr("transform", "translate("+ (w/2) +","+(h-(padding/3))+")")
-  .text("Date"); 
-  
+  .text("Date");
+
 
 
 function Next() {
@@ -142,7 +174,7 @@ function Next() {
       svg.select(".yaxis")
       .transition().duration(2500).ease("sin-in-out")
       .call(yAxis);
-      
+
     d3.selectAll(".rbutton")
       .classed("disabled", true);
     d3.selectAll(".lbutton")
@@ -151,33 +183,7 @@ function Next() {
     	.classed("invisible", true);
     d3.selectAll(".inhalt2")
     	.classed("invisible", false);
-    
-    d3.selectAll("circle")
-      .transition()
-      .duration(2500).ease("sin-in-out")    
-      .attr("cx", function(d){return xScale(getDate(d));})
-      .attr("cy", function(d){return yScale(d.summe);})
-      .attr("r", function(d){return Math.sqrt(d.summe*20);})
-      .style("fill", function(d){if (d.summe < 1) {return "#42FF48";} else if (d.summe < 5) {return "#05C70B";} 				else if (d.summe < 10) {return "#33FFFF";} else if (d.summe < 30) {return "#1BC0C0";} else if (d.summe < 					60) {return "#2FA2F7";} else {return "#0A30CA";}})
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
-  ;}
-  
- function Previous() {
-			yScale.domain([0,17])
-      svg.select(".yaxis")
-      .transition().duration(2500).ease("sin-in-out")
-      .call(yAxis);
-      
-    d3.selectAll(".lbutton")
-      .classed("disabled", true);
-    d3.selectAll(".rbutton")
-    	.classed("disabled", false);
-    d3.selectAll(".inhalt1")
-    	.classed("invisible", false);
-    d3.selectAll(".inhalt2")
-    	.classed("invisible", true);
-    
+
     d3.selectAll("circle")
       .transition()
       .duration(2500).ease("sin-in-out")
@@ -187,7 +193,33 @@ function Next() {
       .style("fill", function(d){if (d.summe < 1) {return "#42FF48";} else if (d.summe < 5) {return "#05C70B";} 				else if (d.summe < 10) {return "#33FFFF";} else if (d.summe < 30) {return "#1BC0C0";} else if (d.summe < 					60) {return "#2FA2F7";} else {return "#0A30CA";}})
       .on('mouseover', tip.show)
       .on('mouseout', tip.hide);
-  ;} 
+  ;}
+
+ function Previous() {
+			yScale.domain([0,17])
+      svg.select(".yaxis")
+      .transition().duration(2500).ease("sin-in-out")
+      .call(yAxis);
+
+    d3.selectAll(".lbutton")
+      .classed("disabled", true);
+    d3.selectAll(".rbutton")
+    	.classed("disabled", false);
+    d3.selectAll(".inhalt1")
+    	.classed("invisible", false);
+    d3.selectAll(".inhalt2")
+    	.classed("invisible", true);
+
+    d3.selectAll("circle")
+      .transition()
+      .duration(2500).ease("sin-in-out")
+      .attr("cx", function(d){return xScale(getDate(d));})
+      .attr("cy", function(d){return yScale(d.summe);})
+      .attr("r", function(d){return Math.sqrt(d.summe*20);})
+      .style("fill", function(d){if (d.summe < 1) {return "#42FF48";} else if (d.summe < 5) {return "#05C70B";} 				else if (d.summe < 10) {return "#33FFFF";} else if (d.summe < 30) {return "#1BC0C0";} else if (d.summe < 					60) {return "#2FA2F7";} else {return "#0A30CA";}})
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+  ;}
 
 svg.append('svg:image')
 .attr({
